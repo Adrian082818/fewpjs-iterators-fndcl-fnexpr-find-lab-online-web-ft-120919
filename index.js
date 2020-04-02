5ac71da07d50043a9cb807dd83fd537d1826caf6
Adrian082818 <mohabiradrian@gmail.com>
2020-04-02 13:38:56 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let result 
+ array.find(function(obj) {
+  if (obj.result === "W") {
+    result = obj.year 
+  }
+})
+return result 
+};
+
